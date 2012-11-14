4d95a9c23d2b0cee7a227fc88c33b1e875bfe944
David Wright <wrightd@humanrightsfirst.org>
2012-11-14 12:24:41 -0500

    Added Debugging
    Just need to see any php specific errors related to outputting parts of
    the unserialized vimeo array

--- vidwebapi.php
+++ vidwebapi.php
@@ -1,6 +1,8 @@
 <?php
 // Video API Beta (for Vimeo)
-
+// Adding debugging - 11/14/12
+error_reporting(E_ALL);
+ini_set('display_errors', True);
 	$vurl= "http://vimeo.com/api/v2/video/53082916.php";	// URL to video.
 	function get_video ($vurl) {
 		$vinit = curl_init($vurl);
